87c2724964cea7c8431968dc2009e65dc40dff94
root <root@DESKTOP-1PF4AT3.localdomain>
2022-10-19 08:00:30 +0900

    first

---
+++ nvim.sh
@@ -0,0 +1,10 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mv nvim.appimage /usr/local/bin/nvim
+
+mkdir ~/.config/nvim
+mkdir ~/.local/share/nvim/plugged
+mkdir ~/.local/share/nvim/site/autoload
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dir \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+git clone git@github.com:PorcoRosso85/dotfiles.git
